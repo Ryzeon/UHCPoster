67627806cccc8630ad0bd3374ee6fe80d0b8d70a
Alex Avila <alex.ramon2004@gmail.com>
2021-05-16 15:22:00 -0500

    Done tweet :d

--- botThread.py
+++ botThread.py
@@ -16,14 +16,19 @@ class tweetThread(threading.Thread):
         print("Successfully connection to @" + str(api.me().screen_name))
         user_List = api.get_list(list_id=LIST_ID)
         print("Found list: " + user_List.name)
+        memberList = []
         for member in api.list_members(list_id=LIST_ID):
+            memberList = []
             print("Found a member: " + member.screen_name)
         print("Load list total members: " + str(user_List.member_count))
         while True:
             print("Search tweets")
             for status in tweepy.Cursor(api.search, q="time.is/EST").items():
+                print(status)
                 try:
-                    print("xd")
+                    if status.user.screen_name in memberList:
+                        print("Posting tweet of" + status.user.screen_name)
+                        api.update_status(status.text)
                 except tweepy.TweepError as ex:
                     print(ex.reason)
             time.sleep(120)  # 2 minutes
